852f02de097ec606ea249d5f57f9eedcbd972c4a
Théo Blandin <feozard@gmail.com>
2025-12-29 15:56:00 +0100

    attempt fix 404

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa"
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
@@ -13,4 +14,12 @@ export default defineConfig({
     host: true
   },
   base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 })
