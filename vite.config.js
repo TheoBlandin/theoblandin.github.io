70c27099e021507cfb525a52ac628b9ecd6307ec
Théo Blandin <feozard@gmail.com>
2025-12-29 15:31:47 +0100

    add githubPagesSpa package

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa"
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    githubPagesSpa(),
   ],
   server: {
     host: true
